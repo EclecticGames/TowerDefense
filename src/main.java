52900f680910310e2131a52414eb73789affd6b1
Pombarzocker <verwendet1@web.de>
2014-05-08 17:53:57 +0200

    slick support
    ich hab versucht slick einzubauen. hat noch nicht ganz geklappt hab
    jetzt aber keine Zeit mehr für heute. Außerdem hab ich nen neuen Branch
    angelegt, damit wir nicht immer die Sachen von anderen überschreiben.

--- src/main.java
+++ src/main.java
@@ -1,9 +1,35 @@
 
-public class main {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.util.Log;
 
-	public static void main(String[] args) {
+
+public class main extends StateBasedGame{
+	
+	public static AppGameContainer appgc;
+
+	public static int screenWidth = 960;
+	public static int screenHeight = 540;
+	
+	public main(String name) {
+		super(name);
+		try{
+			//window wich hold game
+			appgc = new AppGameContainer(new main("test"));
+			appgc.setDisplayMode(screenWidth, screenHeight, false);
+			appgc.start();
+		}catch(SlickException e){
+			Log.error(e.toString());
+		}
+	}
+
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
